6f86dc96b56b5b389dd886c11245b72e69bb740f
Артем Шабуневич <wouhnyua@gmail.com>
2025-12-27 21:48:58 +0200

    Initial React + Express CRUD

---
+++ backend/data/users.js
@@ -0,0 +1,8 @@
+let users = [
+    { id: 1, name: "Nikita", email: "nikita@mail.com" },
+    { id: 2, name: "Artem", email: "artem@mail.com" },
+    { id: 3, name: "Igor", email: "igor@mail.com" },
+    { id: 4, name: "Boris", email: "boris@mail.com" },
+    { id: 5, name: "Oleksiy", email: "oleksiy@mail.com" },
+    { id: 6, name: "Roman", email: "roman@mail.com" }
+];
---
+++ backend/index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/users", (req, res) => {
+    res.json(users);
+});
+
+app.post("/users", (req, res) => {
+    const newUser = {
+        id: Date.now(),
+        name: req.body.name,
+        email: req.body.email
+    };
+    users.push(newUser);
+    res.json(newUser);
+});
+
+app.put("/users/:id", (req, res) => {
+    const id = Number(req.params.id);
+    users = users.map(u =>
+        u.id === id ? { ...u, ...req.body } : u
+    );
+    res.json({ success: true });
+});
+
+app.delete("/users/:id", (req, res) => {
+    const id = Number(req.params.id);
+    users = users.filter(u => u.id !== id);
+    res.json({ success: true });
+});
+
+app.listen(3001, () => {
+    console.log("API running on http://localhost:3001");
+});
